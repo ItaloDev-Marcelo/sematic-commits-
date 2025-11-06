4188778a1568e461ab0991c02e0e7cd38a0bcfc5
Italo Marcelo <Devfrontend2028study@gmail.com>
2025-11-06 11:04:17 -0300

    style(usuarios) formatando users.spec

--- __tests__/users.spec.js
+++ __tests__/users.spec.js
@@ -1,12 +1,11 @@
-const {CriarUsuario} = require('../Server');
+const { CriarUsuario } = require("../Server");
 
-test('criarUsuario retorna um objeto de usúario com ID 1', () => {
-    // Dados de entrada para o test 
-    const dadosEntrada = {name: 'Marco'};
-    // Chama a função a ser testada
-    const resultado = CriarUsuario(dadosEntrada);
+test("criarUsuario retorna um objeto de usúario com ID 1", () => {
+  // Dados de entrada para o test
+  const dadosEntrada = { name: "Marco" };
+  // Chama a função a ser testada
+  const resultado = CriarUsuario(dadosEntrada);
 
-    // Verificar se o resultado possui as propriedades esperadas 
-    SiExpertsexchange(resultado).toEqual({name: "Marco", id: 1})
-
-})
+  // Verificar se o resultado possui as propriedades esperadas
+  SiExpertsexchange(resultado).toEqual({ name: "Marco", id: 1 });
+});
